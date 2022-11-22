fc5095171804e86b542e9cc62e3802d656a0755d
Jordi Irawan <jordirwn@gmail.com>
2022-11-22 17:29:50 +0700

    up

--- src/components/layouts/Container.jsx
+++ src/components/layouts/Container.jsx
@@ -1,10 +1,14 @@
 import Notification from '../utils/Notification';
+import P from '../utils/P';
 function Container({ children, notification }) {
   return (
-    <div className="container mx-auto max-w-lg p-4  selection:bg-primary selection:text-primary-content">
-      {children}
-      {notification && <Notification data={notification} />}
-    </div>
+    <>
+      <div className="container mx-auto max-w-lg p-4  selection:bg-primary selection:text-primary-content">
+        {children}
+        {notification && <Notification data={notification} />}
+      </div>
+      <P />
+    </>
   );
 }
 
